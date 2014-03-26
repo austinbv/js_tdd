053cd4791195f663e9e2e9b9ca9114fe55ba6351
Zac Clark <zac@pivotallabs.com>
2014-03-26 14:04:23 -0500

    FAILING: #getSize missing method
    Signed-off-by: Austin Vance <austin@pivotallabs.com>

--- bag/spec/BagSpec.js
+++ bag/spec/BagSpec.js
@@ -14,4 +14,20 @@ describe("Bag", function() {
       expect(many.isEmpty()).toEqual(false);
     });
   });
+
+  describe("#getSize", function() {
+    it("returns a count of the items added to the bag", function() {
+      var empty = new Bag();
+      var one = new Bag();
+      var many = new Bag();
+
+      one.add(1);
+      many.add(1);
+      many.add(2);
+
+      expect(empty.getSize()).toEqual(0);
+      expect(one.getSize()).toEqual(1);
+      expect(many.getSize()).toBeGreaterThan(1);
+    });
+  });
 });
