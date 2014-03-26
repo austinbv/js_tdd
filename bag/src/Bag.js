3ddc5d778dd24297c2c2d12a04b39906b5477296
Zac Clark <zac@pivotallabs.com>
2014-03-26 18:39:38 -0500

    PASSING: naive approach to #contains
    Signed-off-by: Austin Vance <austin@pivotallabs.com>

--- bag/src/Bag.js
+++ bag/src/Bag.js
@@ -1,4 +1,5 @@
 function Bag() {
+  this._items = [];
   this._size = 0;
 }
 
@@ -6,11 +7,18 @@ Bag.prototype.isEmpty = function() {
   return this._size == 0;
 };
 
-Bag.prototype.add = function () {
+Bag.prototype.add = function (item) {
   this._size++;
+  this._items.push(item);
 };
 
 Bag.prototype.getSize = function() {
   return this._size;
 };
 
+Bag.prototype.contains = function(item) {
+  for(var i = 0; i < this._size; i++) {
+     if (this._items[i] == item) return true;
+  }
+  return false;
+};
